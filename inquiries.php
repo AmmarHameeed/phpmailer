5dff7cc4d6c53107139fe7f5948dc23310cece0f
Ammar Hameed <ammarro444@gmail.com>
2022-01-12 02:47:11 +0500

    First commit
    First commit

---
+++ inquiries.php
@@ -0,0 +1,326 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="icon" href="images/fav-icon.png" type="image/x-icon" />
+	<title>Al-Abrar Combined General Trading Co. | HVAC Division - Contact</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,700" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	<link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
+	
+	<style type="text/css">
+		.gg {
+			background-color: #00a55c;
+			color: white;
+		}
+
+		.gg:hover {
+			background-color: #D3D3D3;
+			color: black;
+			border-radius: 10px;
+		}
+	</style>
+</head>
+
+<body>
+
+	<div class="colorlib-loader"></div>
+<!-- 
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-2">
+							<div id="colorlib-logo"><a href="index.html"><img src="images/logo.png"
+										style="margin-top: -35px;"></div>
+						</div>
+						<div class="col-xs-10 text-right menu-1">
+							<ul>
+								<li class="active"><a href="index.html">Home</a></li>
+								<li><a href="about.html">About</a></li>
+								<li><a href="services.html">Services</a></li>
+								<li><a href="work.html">Projects</a></li>
+								<li><a href="contact.html">Contact</a></li>
+								<li><a href="inquiries.php">Inquiries</a></li>
+								<li><a href="careers.php">Careers</a></li>
+								<li class="has-dropdown">
+									<a href="#">Other Departments</a>
+									<ul class="dropdown">
+										<li><a href="#">Electrical Department</a></li>
+										<li><a href="#">Low Voltage</a></li>
+										<li><a href="#">Duct Manufacturing Factory</a></li>
+										<li><a href="#">Trading (Showroom)</a></li>
+										<li><a href="#">HVAC Maintenance Department</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+					<li style="background-image: url(images/img4.jpg);">
+						<div class="overlay"></div>
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-md-8 col-sm-12 col-md-offset-2 slider-text">
+									<div class="slider-text-inner text-center">
+										<h2>Have an inquiry?</h2>
+										<h1>Inquiries</h1>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</aside>
+
+		<div id="colorlib-contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1 animate-box">
+						<h2>Inquiries</h2>
+						<div class="row contact-info-wrap">
+							<div class="col-md-12">
+							Have an inquiry? Send us an email and our team will get back to you as soon as possible.
+							</div>
+						</div>
+					</div>
+					<div class="col-md-10 col-md-offset-1 animate-box">
+						<form method="post" action="" enctype="multipart/form-data">
+							<div class="row form-group">
+								<div class="col-md-12">
+									<label for="fname">Full Name</label>
+									<input type="text" name="name" id="fname" class="form-control" placeholder="Full Name" required> 
+								</div>
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12">
+									<label for="email">Email</label>
+									<input type="email" name="email" id="email" class="form-control"
+										placeholder="Your email address" required>
+								</div>
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12">
+									<label for="phone">Phone</label>
+									<input type="text" name="phone" id="phone" class="form-control" placeholder="Your phone number" required>
+								</div>
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12">
+									<label for="inquiry">General Inquiry</label>
+									<select class="form-control" name="general_inquiry">
+										<option value="1">Business</option>
+										<option value="2">Technical</option>
+										<option value="3">Special request</option>
+										<option value="4">Career</option>
+									</select>
+								</div>
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12">
+									<label for="message">Your message</label>
+									<textarea name="message" id="message" cols="30" rows="10" class="form-control"
+										placeholder="Write the query you want to ask us." required></textarea>
+								</div>
+							</div>
+							<div class="">
+								<div class="form-group">
+									<input type="submit" name="submit" class="btn gg" value="SUBMIT">
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<footer id="colorlib-footer" role="contentinfo">
+			<div class="container">
+				<div class="row row-pb-md">
+					<div class="col-md-3 colorlib-widget">
+						<h4>About Company</h4>
+						<p>We supply, install, commission, maintain & repair all kinds of A/C units for industrial,
+							private, Residential & Ministries Building, Villas, Hospitals Complexes Five Star Hotel,
+							Super Markets Companies.</p>
+					</div>
+
+					<div class="col-md-4 col-md-push-1 text-center">
+						<h4>Quick Links</h4>
+						<ul class="colorlib-footer-links">
+							<li>
+								<a href="services.html">
+									Services
+								</a>
+							 </li>
+							<li>
+								<a href="contact.html">
+									Contact Us
+								</a>
+							</li>
+							<li>
+								<a href="inquiries.php">
+									Inquiries
+								</a>
+							</li>
+							<li>
+								<a href="careers.php">
+									Careers
+								</a>
+							</li>
+						</ul>
+
+					</div>
+					<div class="col-md-2 col-md-push-1">
+
+					</div>
+					<div class="col-md-3 col-md-push-1">
+						<h4>Contact Info</h4>
+						<ul class="colorlib-footer-links">
+							<li>PO Box 1234, <br> Kuwait, Kuwait</li>
+							<li><a href="tel://1234567920"><i class="icon-phone"></i> +965 1234 5678</a></li>
+							<li><a href="mailto:info@yoursite.com"><i class="icon-envelope"></i> info@yoursite.com</a>
+							</li>
+							<li><a href="index.html"><i class="icon-location4"></i> yourwebsite.com</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<p>
+							 Designed & Developed by Fatma Yousuf -->
+							<!-- <small class="block">Copyright &copy;
+								<script>document.write(new Date().getFullYear());</script> All rights reserved. -->
+								<!-- Designed & Developed by Fatma Yousuf -->
+						<!-- </p>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div> 
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div> -->
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
+
+
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+                
+
+// // Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// // Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(); // create a new object
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+$mail->Host = "usaveservices.com";
+$mail->Port = 465; // or 587
+$mail->IsHTML(true);
+$mail->Username = "contactform@usaveservices.com";
+$mail->Password = "Secure#1357";
+$mail->SetFrom("contactform@usaveservices.com");
+$mail->Subject = "Inquiry - ";
+$mail->Body = "
+Name: asd<br>
+Email: email<br>
+Phone: phone<br>
+Inquiry Type: general_inquiry<br>
+Message: message";
+$mail->AddAddress("abbas.afzal0810@gmail.com");
+
+ if(!$mail->Send()) {?>
+
+<h2>not sent</h2>
+<?php
+ } else { ?>
+	<h2> bi sent</h2>
+ <?php 
+ }
+            
+        
+	
+
+?>
+</body>
+
+</html>
